b1d35fd45e5175bb5d0fef2bac69ae7817be0f29
zdravkokarabenchev <zdravko_k@mail.bg>
2016-09-26 17:13:55 +0300

    Data Types and Variables - Exercises

---
+++ Labs/ModuleTech/DataTypesAndMethods_Exercises/15_(new)_FastPrimeCheckerRefactor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15__new__FastPrimeCheckerRefactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rangeNumber = int.Parse(Console.ReadLine());
+            for (int num = 2; num <= rangeNumber; num++)
+            {
+                bool trueOrFalse = true;
+                for (int j = 2; j <= Math.Sqrt(num); j++)
+                {
+                    if (num % j == 0)
+                    {
+                        trueOrFalse = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{num} -> {trueOrFalse}");
+            }
+        }
+    }
+}
---
+++ Labs/ModuleTech/DataTypesAndMethods_Exercises/19_(new)_TheaThePhotographer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19__new__TheaThePhotographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPictures = int.Parse(Console.ReadLine());
+            int filterTimePerPicture = int.Parse(Console.ReadLine());
+            double percentageGoodPictures = int.Parse(Console.ReadLine());
+            int uploadingTimePerPicture = int.Parse(Console.ReadLine());
+
+            int allPicturesFilter = allPictures * filterTimePerPicture;
+            int goodPictures = (int)(Math.Ceiling(allPictures * (percentageGoodPictures / 100))) * uploadingTimePerPicture;
+            int totalSeconds = allPicturesFilter + goodPictures;
+
+            TimeSpan time = TimeSpan.FromSeconds(totalSeconds);
+
+            string str = time.ToString(@"d\:hh\:mm\:ss");
+            Console.WriteLine(str);
+        }
+    }
+}
